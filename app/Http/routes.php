606c50bc2a08c92be16701f5fab2a427dd425fa3
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-09-05 11:17:14 -0500

    Added routes for all authentication, registration and the Users controller. Added a login page.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,16 @@ Route::resource('employers', 'EmployerController');
 Route::get('/register', function () {
 	return view('auth.mainregister');
 });
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// Route::get('users/{search}', function($search, Request))
+
+Route::resource('/users', 'UsersController');
