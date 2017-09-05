a27fd401641cd1a4fe244547ff28245b90bc30c4
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-09-05 09:01:08 -0500

    Added changes to the show-Nonprofit Dashboard.

--- resources/views/nonprofit/show.blade.php
+++ resources/views/nonprofit/show.blade.php
@@ -1,19 +1,76 @@
 @extends('layouts.master')
 
 @section('title')
-<title>Dashboard</title>
+<title>Nonprofit Dashboard</title>
 @stop
+
 
 @section('content')
+<style>
+
+h1 {
+    margin-bottom: 40px;
+    text-align: center;
+}
+.col-md-6  {
+    margin-right: 80px;
+
+}
+.btn {
+    margin-bottom: 20px;
+}
+
+
+</style>
     <div class="container">
-        <form method="POST" action="">
-        	{!! csrf_field() !!}
-            <div class="row">
-
-            <h1 class="section-title text-center">Nonprofit Dashboard</h1>
-
-            <div class="col-md-6 col-md-offset-3">
-
-     	</form>
+    	<h1 class="section-title text-center">Nonprofit Dashboard</h1>
+        <br>
+        <a href="">
+        <div class="row">
+            <div class="col-md-6 col-md-3">
+                <div class="event-section">
+                	<h4 class="section-title">My Events Calendar</h4>
+                    <br>
+                    <div class="calendar" > Calendar Placeholder </div>
+<!--                     <img src="/img/september2017.jpg" ></img> -->
+					<img src="{{ asset('september2017.jpg') }}">
+                    <br>
+                    <button class="btn btn-secondary" name="edit-event" > Edit an Event </button>
+                    <br>
+                    <button class="btn btn-secondary" name="add-event" > Add an Event</button>
+                </div>
+            </div>
+            <div class="col-md-6 col-md-3">
+                    <div class="signupByEvent">
+                    	<h4 class="section-title">Signup By Event</h4>
+                        <h4> Event 1 </h4> <div> [Event 1 Placeholder] </div>
+                        <br>
+                        <h4> Event 2 </h4> <div> [Event 2 Placeholder]  </div>
+                        <br>
+                        <h4 class="section-title ">Signup By Date</h1>
+                        <div class="calendar"> Calendar Placeholder </div>
+                    	<img src="/img/september2017.jpg" ></img>
+                    	<br>
+                    </div>
+            </div>
+            <div class="col-md-6 col-md-3">
+                    <div class="attendance">
+                        <h4> Validate Attendance </h4>
+                        <br>
+                        <div> Attendance List Placeholder </div>
+                        <br>
+                        <button class="btn btn-success" name="validate"> Validate</button>
+                        <br>
+                        <button class="btn btn-success" name="print"> Print</button>
+                        <h4> Feedback </h4>
+                        <button class="btn btn-success" name="submitFeedback"> Submit Feedback on an Employee</button>
+                    </div>
+            </div>
+        </div>
     </div>
+
+
 @stop
+
+
+                       
