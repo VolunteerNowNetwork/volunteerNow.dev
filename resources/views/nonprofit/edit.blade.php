91f86799c299afe0231484e61bc7c19a0c4127f8
Analyssa Martinez <analyssamartinez@Analyssas-MBP.satx.rr.com>
2017-09-04 21:36:33 -0500

    Added more changes to Nonprofit edit and show pages

--- resources/views/nonprofit/edit.blade.php
+++ resources/views/nonprofit/edit.blade.php
@@ -4,34 +4,35 @@
 <title>Edit Account</title>
 @stop
 
-
 @section('content')
-    <main class="container">
-<!--    {!! $errors->first('title', '<span class="help-block">:message</span>') !!}
-        {!! $errors->first('url', '<span class="help-block">:message</span>') !!}
-        {!! $errors->first('content', '<span class="help-block">:message</span>') !!} -->
-
+    <div class="container">
         <form method="POST" action="">
-<!--        "{{ action('NonprofitController@update', $nonprofit->id)" -->
             {!! csrf_field() !!}
-            <h1>Edit Your account</h1>
-            Name of Nonprofit: <input type="text" name="nonprofitname">
-            <br>
-            Admin Name: <input type="text" name="nonprofitadmin">
-            <br>
-            Username: <input type="text" name="nonprofitusername">
-            <br>
-            Bio About Your Nonprofit?
-            <br>
-            <textarea name="nonprofitbio" rows="5" cols="40"></textarea>
-            <br>
+            <div class="row">
+
+            <h1 class="section-title text-center">Update Your Account</h1>
+
+            <div class="col-md-6 col-md-offset-3">
+            <div class="form-group">
+                Name of Nonprofit: <input type="text" class="form-control" id="nonprofitname" name="nonprofitname" placeholder="" data-required>
+            </div>
+            <div class="form-group">
+                Admin Name: <input type="text" class="form-control" id="nonprofitadmin" name="nonprofitadmin" placeholder="" data-required>
+            </div>
+            <div class="form-group">
+                Username: <input type="text" class="form-control" id="nonprofitusername" name="nonprofitusername" placeholder="" data-required>
+            </div>
+            <div class="form-group">
+                Bio About Your Nonprofit: <textarea name="nonprofitbio" rows="3" cols="64">{{ old('nonprofitbio') }}</textarea>
+            </div>
         <!-- File Photo Input -->
             <div class="form-group">
                 <label for="image">Upload Image</label>
                 <input type="file" name="image" id="image">
             </div>
-<!--             {{ method_field('PUT') }} -->
+            <!-- {{ method_field('PUT') }} -->
             <button>Update</button>
         </form>
-    </main>
+    </div>
 @stop
+
---
+++ resources/views/nonprofit/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title')
+<title>Dashboard</title>
+@stop
+
+@section('content')
+    <div class="container">
+        <form method="POST" action="">
+        	{!! csrf_field() !!}
+            <div class="row">
+
+            <h1 class="section-title text-center">Nonprofit Dashboard</h1>
+
+            <div class="col-md-6 col-md-offset-3">
+
+     	</form>
+    </div>
+@stop
