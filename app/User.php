055a70e4413a50af7181e7ddba1d78c2f8ec2218
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-09-06 14:48:26 -0500

    Made changes to User and Auth Controller to get Nonprofit Form to submit

--- app/User.php
+++ app/User.php
@@ -29,7 +29,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'organizationName', 'admin', 'username', 'contactNumber', 'websiteLink', 'employerId', 'employeeId', 'nonProfitId', 'taxId', 'bio', 'availableHours', 'additionalResources', 'benefit', 'corporateSponsor', 'withWho'];
 
     /**
      * The attributes excluded from the model's JSON form.
