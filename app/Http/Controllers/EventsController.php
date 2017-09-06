aaccb882d128d2565bfc24f8dcad228e3a5397af
Kristie Rodriguez <pocketkristi@hotmail.com>
2017-09-06 16:37:42 -0500

    Updated routes and Events Controller

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class EventController extends Controller
+class EventsController extends Controller
 {
     /**
      * Display a listing of the resource.
