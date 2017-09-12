61af8964e0ed580bbb1758998c984f0fd54b341b
Jen Ayala <jenayala1@gmail.com>
2017-09-11 21:24:12 -0500

    employer edit saving to db

--- resources/views/employer/edit.blade.php
+++ resources/views/employer/edit.blade.php
@@ -34,44 +34,44 @@ h1 {
                     <input type="hidden" name="user_group" value="employee">
                     <div class="form-group">
                     Company Name    <input type="text" class="form-control" id="nameOfCompany" name="nameOfCompany" placeholder="NameOfCompany"
-                        data-required>
+                        data-required> {{ old('organization_name') }}
                     </div>
                      {!! $errors->first('password', '<div class="alert alert-danger">:message</div>') !!}
                     <div class="form-group">
-                    Password    <input type="password" class="form-control" id="" name="password" placeholder="Password" data-required>
+                    Password    <input type="password" class="form-control" id="" name="password" placeholder="Password" data-required> {{ old('password') }}
                     </div>
                     {!! $errors->first('confirmPassword', '<div class="alert alert-danger">:message</div>') !!}
                     <div class="form-group">
-                    Confirm Password    <input type="confirmPassword" class="form-control" id="" name="confirmPassword" placeholder="Password" data-required>
+                    Confirm Password   Password  <input type="password" class="form-control" id="" name="password" placeholder="Password" data-required>
                     </div>
                     <div class="form-group">
-                    Admin Name    <input type="admin" class="form-control" id="" name="admin">
+                    Admin Name    <input type="admin" class="form-control" id="" name="admin"> {{ old('admin') }}
                     </div>
                     <div class="form-group">
-                    Username   <input type="username" class="form-control" id="" name="username">
+                    Username   <input type="username" class="form-control" id="" name="username"> {{ old('username') }}
                     </div>
                     <div class="form-group">
-                    Company Website Link    <input type="websiteLink" class="form-control" id="" name="websiteLink">
+                    Company Website Link    <input type="websiteLink" class="form-control" id="" name="websiteLink">{{ old('website_link') }}
                     </div>
                     <div class="form-group">
-                    Tax I.D.  <input type="taxId" class="form-control" id="" name="taxId">
+                    Tax I.D.  <input type="taxId" class="form-control" id="" name="taxId"> {{ old('tax_id') }}
                     </div>
                     <div class="form-group">
                         About Your Company <textarea name="bio" rows="3" cols="64">{{ old('bio') }}</textarea>
                     </div>
 
                     <div class="form-group">
-                    Are you a corporate sponsor?  <input type="corporateSponsor" class="form-control" id="corporateSponsor" name="corporateSponsor">
+                    Are you a corporate sponsor?  <input type="corporateSponsor" class="form-control" id="corporateSponsor" name="corporateSponsor">{{ old('corporate_sponsor') }}
                     </div>
                     <div class="form-group">
-                        <textarea type="additionalResources" class="form-control" id="" name="additionalResources"> Aditional Resources </textarea>
+                    Aditional Resources   <textarea type="additionalResources" class="form-control" id="" name="additionalResources"> {{ old('additional_resources') }}</textarea>
                     </div>
                     <div class="row">
                         <div class="col-sm-6">
                             Photo   <input type="image" class="form-control" id="image" name="image">
                             <br>
                             {{ method_field('PUT') }}
-                            <button type="Registar" class="btn btn-primary">Registar</button>
+                            <button type="update" class="btn btn-primary">Update</button>
                         </div>
                     <br>
                     <div class="form-group">
