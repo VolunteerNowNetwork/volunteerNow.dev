733134950331aec620c5c53319dfbce7e09b1aad
Jen Ayala <jenayala1@gmail.com>
2017-09-13 13:16:13 -0500

    Updates to about us blade

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -12,23 +12,25 @@
             <h1 class="text-center">About Volunteer Now!</h1>
             <h3 class="text-center">The power of community connection...YOU make the difference and WE make it easy.</h3>
               <br>
+              <p> We provide a convenient forum to enable employers to offer time off for employee volunteers at events with participating non-profit organizations. </p>
+
 
               <div class="row">
                   <div class="col-md-6 col-md-3">
                         <div class="contact-section">
                             <div name="Kristie">
                                 <h4> Kristie Rodriguez </h4>
-                                <img src="/img/mainphoto.png">
+                                <img src="/img/mainphoto.png" width="300" height="300">
                             </div>
                             <br>
                             <div name="Analyssa">
                                 <h4> Analyssa Martinez</h4>
-                                <img src="/img/mainphoto.png">
+                                <img src="/img/mainphoto.png" width="300" height="300">
                             </div>
                             <br>
                             <div name="Jennifer">
                                 <h4> Jennifer Ayala</h4>
-                                <img src="/img/mainphoto.png">
+                                <img src="/img/mainphoto.png" width="300" height="300">
                             </div>
                         </div>
                     </div>
