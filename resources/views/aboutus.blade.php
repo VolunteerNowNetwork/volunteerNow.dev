9e7c98b7c8415c972ec93f2b212aef18e45ce0be
Jen Ayala <jenayala1@gmail.com>
2017-09-13 13:07:36 -0500

    created about us blade and added to routes

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+
+@section('title')
+<title>About Volunteer Now</title>
+@stop
+
+@section('content')
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="container">
+            <h1 class="text-center">About Volunteer Now!</h1>
+            <h3 class="text-center">The power of community connection...YOU make the difference and WE make it easy.</h3>
+              <br>
+
+              <div class="row">
+                  <div class="col-md-6 col-md-3">
+                        <div class="contact-section">
+                            <div name="Kristie">
+                                <h4> Kristie Rodriguez </h4>
+                                <img src="/img/mainphoto.png">
+                            </div>
+                            <br>
+                            <div name="Analyssa">
+                                <h4> Analyssa Martinez</h4>
+                                <img src="/img/mainphoto.png">
+                            </div>
+                            <br>
+                            <div name="Jennifer">
+                                <h4> Jennifer Ayala</h4>
+                                <img src="/img/mainphoto.png">
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    </body>
+</html>
+@stop
