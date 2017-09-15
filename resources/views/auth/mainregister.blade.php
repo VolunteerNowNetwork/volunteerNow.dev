0c1e9628f78145807a66a655168a3fb5c9a8b0b7
Kristie Rodriguez <pocketkristi@hotmail.com>
2017-09-15 15:55:31 -0500

    Added Jumbotron to register page...continue to work on styling

--- resources/views/auth/mainregister.blade.php
+++ resources/views/auth/mainregister.blade.php
@@ -2,6 +2,14 @@
 
 @section('title')
 <title>Register</title>
+<div class="container">
+  <div class="jumbotron">
+    <h1>Volunteer Now!</h1> 
+    <img src="img/mainphoto.png" class="img-responsive" alt="Responsive image" style="width: 50%; height: 20%; padding: 1%">
+  <p id="inspirQuote"><strong>"For nonprofit organizations, few resources provide more value than volunteers. Many nonprofits would not be able to survive—let alone thrive—without the work of volunteers."<strong></p> 
+    
+  </div>
+</div>
 @stop
 
 
@@ -11,17 +19,21 @@
             <div class="row">
                 <div class="form-group">
                     <h1 class="section-title text-center">Register As A</h1>
-
-                    <div class="col-md-6 col-md-offset-3">
-                    <a class="text-center" href="/nonprofit/create">Nonprofit</a>
+                    <div class="col-md-6 col-md-offset-3"> 
+                    <div class="form-group">   
+                        <a class="text-center" href="/nonprofit/create">Nonprofit</a>
+                    </div>
                     <br>
-                    <a class ="text-center" href="/employer/create">Employer</a>
+                    <div class="form-group"> 
+                        <a class ="text-center" href="/employer/create">Employer</a>
+                    </div>
                     <br>
-                    <a class ="text-center" href="/employee/create">Employee</a>
-                    <br>
+                    <div class="form-group"> 
+                        <a class ="text-center" href="/employee/create">Employee</a>
+                    </div>
                     <br>
                     <a href="auth/login" class="btn btn-success">Go To Login</a>
-                    </div>
+                    
                 </div>
             </div>
         </form>
