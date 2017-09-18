c93c2b2c235474baec952941effb6224f561bd15
Jen Ayala <jenayala1@gmail.com>
2017-09-17 20:49:25 -0500

    added pic to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('title')
-<title></title>
+<title>Login</title>
 @stop
 @section('content')
 <style>
@@ -13,66 +13,76 @@
 
     h1 {
         font-size: 30px;
+        color: black;
         margin-top: 30px;
         margin-bottom: 30px;
         text-align: center;
     }
-    .text-center {
-        font-size: 20px;
-        text-align: center;
-    }
     p {
         font-size: 16px;
-        text-align: left;
+        color: black;
+        text-align: center;
         margin-bottom: 20px;
         margin-top: 15px;
     }
-
-    .photo{
-      width: 75%;
-      height: 75%;
-      margin-left: 80px;
-      margin-top: 40px;
+    img.photo{
+      width: 100%;
       opacity: 40%;
+      margin-left: 15px;
     }
-
+    div.description{
+      position:absolute;
+      bottom: 210px;
+      left:0px;
+      width:100%;
+      color:white;
+      opacity: 60%;
+      filter:alpha(opacity=90);
+    }
+    p.description_content{
+      padding:0px;
+      margin:0px;
+      font-size: 40px;
+    }
 </style>
-<div class="container">
-    <section id="login">
-        <div class="row">
-            <h1 class="section-title text text-center"> Please Log In </h1>
-            <br>
-            <div class="col-md-6 col-md-offset-3">
-                <div class='text-center'>The power of community connection!</div>
-                <br>
-                <p>Login with your Email and Password</p>
-                <form method="POST" action="{{ action('Auth\AuthController@postLogin') }}">
-                    {!! csrf_field() !!}
 
-                     {!! $errors->first('email', '<div class="alert alert-danger">:message</div>') !!}
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="email" name="email" placeholder="Email" data-required>
-                    </div>
-                     {!! $errors->first('password', '<div class="alert alert-danger">:message</div>') !!}
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
-                    </div>
-                  <div>
-                        <input type="checkbox" name="remember" >   Remember Me
-                    </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <button type="submit" class="btn btn-primary"> Login </button>
-                        </div>
-                        <div class="col-sm-6 text-right">
-                            <a href="/register" class="btn btn-success"> Register </a>
-                        </div>
-                    </div>
-                </form>
-            </div>
+<div class='wrapper'>
+
+  <img class="photo" src="/img/hsc.jpg" />
+    <div class='description'>
+    <h1>Login with your Email and Password</h1>
+    <form method="POST" action="{{ action('Auth\AuthController@postLogin') }}">
+      {!! csrf_field() !!}
+      <div class="container">
+          <section id="login">
+              <div class="row">
+                  <br>
+                  <div class="col-md-6 col-md-offset-3">
+                           {!! $errors->first('email', '<div class="alert alert-danger">:message</div>') !!}
+                          <div class="form-group">
+                              <input type="text" class="form-control" id="email" name="email" placeholder="Email" data-required>
+                          </div>
+                           {!! $errors->first('password', '<div class="alert alert-danger">:message</div>') !!}
+                          <div class="form-group">
+                              <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
+                          </div>
+                          <div>
+                              <input type="checkbox" name="remember" >   Remember Me
+                          </div>
+                          <br>
+                          <div class="row">
+                              <div class="col-sm-6">
+                                  <button type="submit" class="btn btn-primary"> Login </button>
+                              </div>
+                              <div class="col-sm-6 text-right">
+                                  <a href="/register" class="btn btn-success"> Register </a>
+                              </div>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
         </div>
     </section>
-</div>
-
+    </div>
 @stop
