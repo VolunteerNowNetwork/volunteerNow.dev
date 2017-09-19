73457ef3070c786a4b6ec2752b09aac3c84f0eed
Jen Ayala <jenayala1@gmail.com>
2017-09-19 09:46:18 -0500

    Changed styling on non-profit edit to match employee

--- resources/views/nonprofit/edit.blade.php
+++ resources/views/nonprofit/edit.blade.php
@@ -1,10 +1,46 @@
 @extends('layouts.master')
 
 @section('title')
-<title>Edit Your Account Info</title>
+<title>Edit Nonprofit</title>
+<html>
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="https://static.filestackapi.com/v3/filestack.js"></script>
+<script>
+    var client = filestack.init('AWfpMt9vqSzalYlBfIu2tz');
+    function showPicker() {
+        client.pick({
+        }).then(function(result) {
+            console.log(JSON.stringify(result.filesUploaded));
+            var handle = result.filesUploaded[0].handle;
+            console.log(handle);
+            $("#filestack").attr('src', 'https://process.filestackapi.com/' + handle);
+            $("input[id=image]").val(handle);
+        });
+    }
+</script>
 @stop
 
 @section('content')
+<!DOCTYPE html>
+
+<style>
+
+body{
+    background-color: #D3D3D3;
+    font-family: 'Work Sans', sans-serif;
+}
+h1 {
+    text-align: center;
+}
+.container{
+	background-color:white;
+	margin-top:20px;
+	border: 3px solid #10D0AB;
+	border-radius: 25px;
+	height:90%;
+	width:70%;
+}
+</style>
     <div class="container">
         <form method="POST" action="{{ action('NonprofitController@update', $user->id) }}">
             {!! csrf_field() !!}
@@ -14,20 +50,49 @@
 
             <div class="col-md-6 col-md-offset-3">
             <div class="form-group">
-                Name of Nonprofit: <input type="text" class="form-control" id="organization_name" name="organization_name" placeholder="" data-required>
+                Name of Nonprofit: <input type="text" class="form-control" id="organization_name" name="organization_name" placeholder="" value="{{ $user->organization_name }}" data-required >
             </div>
             <div class="form-group">
-                Admin Name: <input type="text" class="form-control" id="admin" name="admin" placeholder="" data-required>
+                Admin Name: <input type="text" class="form-control" id="admin" name="admin" placeholder="" value="{{ $user->admin }}" data-required>
             </div>
             <div class="form-group">
-                Username: <input type="text" class="form-control" id="username" name="username" placeholder="" data-required>
+                Username: <input type="text" class="form-control" id="username" name="username" placeholder="" value="{{ $user->username }}"data-required>
             </div>
             <div class="form-group">
-                Bio About Your Nonprofit: <textarea name="bio" rows="3" cols="64">{{ old('bio') }}</textarea>
+                Email <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}">
             </div>
-            {{ method_field('PUT') }}
-            <button>Update</button>
-        </form>
-    </div>
+            <div class="form-group">
+                Password    <input type="password" class="form-control" id="" name="password" placeholder="Password" data-required> {{ old('password') }}
+            </div>
+             <div class="form-group">
+                 Contact Number  <input type="text" class="form-control" id="contact_number" name="contact_number" value="{{ $user->contact_number }}">
+             </div>
+            <div class="form-group">
+                Bio About Your Nonprofit: <textarea type="text" class="form-control" id="" name="bio" value="{{ $user->bio }}">  </textarea>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                        <input type="button" class="btn btn-primary" value="Upload Photo" onclick="showPicker()" ></input>
+                        <input type="hidden" id="image" name="image" value="{{ $user->image }}"></input>
+                        <img id='filestack' src="" name="image" ></img>
+                    {{ method_field('PUT') }}
+                    <br>
+                </div>
+                <br>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-sm-6">
+                    <button type="submit" class="btn btn-success" value="edit user"> Update </button>
+                </div>
+     </form>
+    <form method="POST" action="{{ action('EmployeeController@destroy', $user->id) }}">
+                      {!! csrf_field() !!}
+                <div class="col-sm-6">
+                      <button class="btn btn-danger" value="delete user">DELETE </button>
+                      {{ method_field('DELETE') }}
+                </div>
+    </form>
+        </div>
+     <br>
 @stop
-
